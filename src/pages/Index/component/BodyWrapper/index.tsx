b1e3d4e813d9bbe76ea3b80091567dc43055ebc5
xxxx <>
2022-02-15 01:31:19 +0800

    dev 6

--- src/pages/Index/component/BodyWrapper/index.tsx
+++ src/pages/Index/component/BodyWrapper/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 export default function Aside({ children }: { children: ReactNode }) {
 
   return (
-    <div className='lg:grid lg:grid-cols-12 lg:gap-x-5 p-8'>
+    <div className='lg:grid lg:grid-cols-12 lg:gap-x-2 lg:gap-y-2 p-4'>
       {children}
     </div>
   )
